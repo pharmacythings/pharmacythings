2b9b447a6defdba69df40e4659feaa0e36cc4466
pharmacythings <thewhiterabbit@riseup.net>
2021-10-14 23:08:43 +0100

    Add files via upload

---
+++ pharmacy_quiz/js/questions.js
@@ -0,0 +1,67 @@
+const myQuestions = [
+  {
+    title: "Question 1 - OTC",
+    question: "What does OTC stand for?...<p>Select your answer then Confirm.</p>",
+    background: "<img src='./img/bg_slide1.jpg'>",
+    answers: {
+      a: "Off the cuff",
+      b: "Over the counter",
+      c: "Occupational Therapy Classes."
+    },
+    correctAnswer: "b",
+    correctAnswerText: "That is correct.<p>OTC stands for “over-the-counter,” or nonprescription, medicine. These are medicines available without a healthcare provider’s prescription.</p>",
+    falseAnswerText: "That's not correct.<p>OTC stands for “over-the-counter,” or nonprescription, medicine. These are medicines available without a healthcare provider’s prescription.</p>"
+  },
+  {
+    title: "Question 2 - Saxenda",
+    question: "How long will a Saxenda pen last when taking 'full dose'?<p>Select your answer then Confirm.</p>",
+    background: "<img src='./img/bg_slide1.jpg'>",
+    answers: {
+      a: "24 hours",
+      b: "6 days",
+      c: "30 days"
+    },
+    correctAnswer: "b",
+    correctAnswerText: "That is correct.<p>Once you are taking the 3-mg dose, each pen will last 6 days.</p>",
+    falseAnswerText: "That's not correct.<p>Once you are taking the 3-mg dose, each pen will last 6 days.</p>"
+  },
+  {
+    title: "Question 3 - Royal Mail",
+    question: "What is the maximum weight allowed for a Royal Mail package?</p><p>Select your answer then Confirm.</p>",
+    background: "<img src='./img/bg_slide3.jpg'>",
+    answers: {
+      a: "2kg.",
+      b: "5kg",
+      c: "10kg"
+    },
+    correctAnswer: "a",
+    correctAnswerText: "That is correct.<p>Anything above 2kg should be posted usind DPD</p>",
+    falseAnswerText: "That´s wrong!<p>Anything above 2kg should be posted usind DPD.</p>"
+  },
+  {
+    title: "Question 4 - Assembly point",
+    question: "Do you know our assembly point of Interim II?<p>Select your answer then Confirm.</p>",
+    background: "<img src='./img/bg_slide4.gif'>",
+    answers: {
+      a: "X1",
+      b: "X2",
+      c: "X3"
+    },
+    correctAnswer: "a",
+    correctAnswerText: "That is correct.<p>Our assembly point is opposite the main entrance next to the trees.</p>",
+    falseAnswerText: "That is not correct.<p>X2 is no assembly point and X3 is the assembly point for Interim III.</p>"
+  },
+  {
+    title: "Question 5 - Fire alarm",
+    question: "Almost there...<p>In case of fire. Alarm & warn people in the building! Fire alarms are located always different per department! Do you know the location of the fire alarms in BIOCON?</p><p>Select your answer then Confirm.</p>",
+    background: "<img src='./img/bg_slide5.jpg'>",
+    answers: {
+      a: "Main entrance door",
+      b: "Next to Resilience room",
+      c: "Opposite of woman restroom"
+    },
+    correctAnswer: "c",
+    correctAnswerText: "That is correct.<p>The fire alarm is located opposite of woman restroom. Call fire service - 112! Focus on main questions: WHO is calling? WHAT happened? WHERE did it happen? HOW many are injured? WAIT for questions!</p>",
+    falseAnswerText: "That is not correct.<p>Check the floor before you try again.</p>"
+  }
+];
